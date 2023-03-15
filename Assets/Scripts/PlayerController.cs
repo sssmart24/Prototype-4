00b59cfa231c98cee2b8022c2ca81ac39eec93af
ssmart24 <ssmart24@aoiths.org>
2023-03-15 10:17:49 -0400

    Prototype 4 part 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody> ();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRB.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
